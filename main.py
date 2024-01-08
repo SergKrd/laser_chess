f4295999868b3263fa3ba97dc36ec30975f883ef
Serge <sp.pisanko@gmail.com>
2024-01-08 21:42:45 +0300

    Реализовано меню и пауза.

--- main.py
+++ main.py
@@ -10,19 +10,39 @@ if __name__ == "__main__":
     width, height = size.current_w, size.current_h
     clock = py.time.Clock()
     running = True
+    part = int(height / 8)
 
-    part = main_1.create_board(screen, (255, 255, 255), width, height)
-    print(main_1.d)
-    print(part)
-    py.display.update()
+
+    # main_1.create_board(screen, (255, 255, 255), width, height)
+    # print(main_1.d)
+    # print(part)
+
+    menu = main_1.Menu()
+    menu.append_option('Играть', main_1.create_board(screen, (255, 255, 255), width, height))
+    menu.append_option('Статистика', screen.fill((255, 255, 255)))
+    menu.append_option('Выход', quit)
 
     while running:
         for event in py.event.get():
             if event.type == py.QUIT:
                 running = False
-            if event.type == py.KEYDOWN:
+            elif event.type == py.KEYDOWN:
                 if py.key.get_pressed()[py.K_ESCAPE]:
-                    running = False
+                    screen.fill((255, 255, 102))
+                    menu.draw(screen, part * 4, part * 2, part)
+                    py.display.update()
+                elif py.key.get_pressed()[py.K_w]:
+                    menu.switch(-1)
+                elif py.key.get_pressed()[py.K_s]:
+                    menu.switch(1)
+
+                elif py.key.get_pressed()[py.K_SPACE]:
+                    menu.select()
+
+
+        screen.fill((255, 255, 102))
+        menu.draw(screen, part * 4, part * 2, part)
+        py.display.update()
 
         clock.tick(FPS)
     py.quit()
--- main_1.py
+++ main_1.py
@@ -1,6 +1,33 @@
 import pygame as py
 
+py.font.init()
 d = {}
+ARIAl_50 = py.font.SysFont('arial', 100, 2)
+
+
+class Menu:
+    def __init__(self):
+        self.options_surfaces = []
+        self.callbacks = []
+        self.current_option = 0
+
+    def append_option(self, option, callback):
+        self.options_surfaces.append(ARIAl_50.render(option, True, (0, 0, 0)))
+        self.callbacks.append(callback)
+
+    def switch(self, direction):
+        self.current_option = max(0, min(self.current_option + direction, len(self.options_surfaces) - 1))
+
+    def select(self):
+        self.callbacks[self.current_option]()
+
+    def draw(self, surface, x, y, option_y_padding):
+        for i, option in enumerate(self.options_surfaces):
+            option_rect = option.get_rect()
+            option_rect.topleft = (x, y + i * option_y_padding)
+            if i == self.current_option:
+                py.draw.rect(surface, (0, 100, 0), option_rect)
+            surface.blit(option, option_rect)
 
 
 def create_column(screen, color, x, y, part, count):
@@ -22,4 +49,3 @@ def create_board(screen, color, width, height):
         create_column(screen, color, x, y, part, count)
         count += 8
         x += part
-    return part
